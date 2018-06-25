19028ee93bc5aa6ea836257fbf9e818fb3553d31
RichCadman <33174800+RichCadman@users.noreply.github.com>
2018-06-25 09:38:55 +0800

    更新

---
+++ extend/log/Logs.php
@@ -0,0 +1,32 @@
+<?php
+namespace log;
+use \app\common\model\Log;
+/**
+ * Created by PhpStorm.
+ * ThinkPHP VERSIONS：Think PHP 5.1.5
+ * Author: Mr.liu <417626953@qq.com>
+ * Date: 2018/6/7
+ * Time: 15:38
+ */
+class Logs
+{
+    /**
+     * @param $name 操作人
+     * @param $title 操作内容
+     * @param $type 操作类型
+     * @param $ip IP地址
+     * @param $browser 浏览器信息
+     */
+    //写入日志
+    public static function write($title,$type)
+    {
+        $users = session('admin');
+        $data['name'] = $users['username'];
+        $data['title'] = $title;
+        $data['type'] = $type;
+        $data['ip'] = request()->ip();;
+        $data['browser'] = $_SERVER['HTTP_USER_AGENT'];
+        $data['time'] = date('Y-m-d H:i:s',time());
+        Log::create($data);
+    }
+}
---
+++ extend/my/Test.php
@@ -0,0 +1,18 @@
+<?php
+namespace my;
+/**
+ * Created by PhpStorm.
+ * ThinkPHP VERSIONS：Think PHP 5.1.5
+ * Author: Mr.liu <417626953@qq.com>
+ * Date: 2018/5/25
+ * Time: 11:47
+ */
+//测试类
+class Test
+{
+    //测试方法
+    public function sayHello()
+    {
+        echo 'Hello World!';
+    }
+}
